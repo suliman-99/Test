e2e6526af5025f03f4d1d65c16e82b343fb85cf8
Suliman Awad <sulimanawadstudy@gmail.com>
2023-08-22 18:04:15 +0300

    .

--- core/serializers.py
+++ core/serializers.py
@@ -1,12 +1,21 @@
 from rest_framework import serializers
 from core.models import *
+from Q10 import check_sentence_meaning
+from Q11 import compare_and_visualize_drawing_similarity
 
 class Q10Serializer(serializers.Serializer):
     sentence = serializers.CharField()
+
+    def create(self, request, *args, **kwargs):
+        return {"data": check_sentence_meaning(request.data.get('sentence'))}
 
 
 class Q11Serializer(serializers.ModelSerializer):
     class Meta:
         model = Fake
         fields = ['image']
+
+    def create(self, validated_data):
+        fake = super().create(validated_data)
+        return {"data": compare_and_visualize_drawing_similarity(fake.image.url)}
 
--- core/views.py
+++ core/views.py
@@ -1,21 +1,12 @@
 from rest_framework.response import Response
 from rest_framework.generics import CreateAPIView
 from core.serializers import *
-from Q10 import check_sentence_meaning
-from Q11 import compare_and_visualize_drawing_similarity
 
 
 class Q10View(CreateAPIView):
     serializer_class = Q10Serializer
-    def create(self, request, *args, **kwargs):
-        return Response(check_sentence_meaning(request.data.get('sentence')))
 
 
 class Q11View(CreateAPIView):
     serializer_class = Q11Serializer
-    def create(self, request, *args, **kwargs):
-        fake = super().create(request, *args, **kwargs)
-        return Response(compare_and_visualize_drawing_similarity(fake.image.url))
-
-
 
